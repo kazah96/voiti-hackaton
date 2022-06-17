a06fc4a2531c17f51db6722f007d5cc883a96794
tema <you@example.com>
2022-06-17 06:36:54 +0500

    nginx

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './modules/auth/auth.controller';
 // dotenv.config();
 
 const mongoDbConnectionString = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
-console.log(mongoDbConnectionString)
+console.log(mongoDbConnectionString);
 @Module({
   imports: [
     MongooseModule.forRoot(mongoDbConnectionString),
