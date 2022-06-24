7a12617d3ad046cb272e2e9dfe6aebae52752b6a
Advocad <garifullin@everytale.net>
2022-06-25 04:44:46 +0500

    added token for request

--- react-frontend/src/shared/api/apiClient.ts
+++ react-frontend/src/shared/api/apiClient.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const authToken = localStorage.getItem('accessToken');
+
 const axiosClient = axios.create({
   baseURL: `/api`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
+    ...(authToken ? { authorization: authToken } : {}),
   },
 });
 
