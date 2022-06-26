da506fd5a2cbc42c50552757852b72cb9deda467
tema <you@example.com>
2022-06-26 21:23:58 +0500

    removed unused apps

--- fastapi-backend/src/run.py
+++
@@ -1,8 +0,0 @@
-import os
-import uvicorn
-
-isProduction = 'MODE' in os.environ and os.environ['MODE'] == 'prod'
-
-if __name__ == "__main__":
-    uvicorn.run("server.main:root", host="0.0.0.0",
-                port=5000, reload=not isProduction, log_level="info")
